944051eb73afc49e50f5dfe5da1a999f98271e4d
Maxi Diaz <diaz.burg.maximiliano@gmail.com>
2024-05-08 17:06:54 -0300

    -
    - Separated the models into different files
    - Added information in README to run clearml
    - Added TODO in readme

---
+++ ml/src/keypoint_generator.py
@@ -0,0 +1,47 @@
+import itertools
+
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+import pandas as pd
+
+
+class KeypointGenerator:
+    def __init__(self, model_path):
+        self.detector = self.__initialize_detector(model_path)
+
+    def generate_keypoints(self, output_file):
+        landmarks = []
+        for letter in ['L', 'O', 'K', 'I']:
+            for i in range(1, 3001):
+                data_landmark = self.create_landmark(f'../datasets/asl_alphabet_train/{letter}/{letter}{i}.jpg')
+                if data_landmark is not None:
+                    data_landmark.append(letter)
+                    landmarks.append(data_landmark)
+        columns = [f'x_{i}' for i in range(63)]
+        columns.append('class')
+        dataframe = pd.DataFrame(data=landmarks, columns=columns)
+        dataframe.to_csv(path_or_buf=output_file)
+
+    def create_landmark(self, image_path):
+        image = mp.Image.create_from_file(image_path)
+        detection_result = self.detector.detect(image)
+        return self.process_landmark(detection_result)
+    
+
+    def process_landmark(self, detection_result):
+        if not detection_result.hand_landmarks:
+            return None
+        landmarks = detection_result.hand_landmarks[0]
+
+        landmark_extraction = lambda keypoint: [keypoint.x, keypoint.y, keypoint.z]
+        landmarks = list(map(landmark_extraction, landmarks))
+        landmarks = list(itertools.chain.from_iterable(landmarks))
+        return landmarks
+    
+
+    def __initialize_detector(self, model_path):
+        base_options = python.BaseOptions(model_asset_path=model_path)
+        options = vision.HandLandmarkerOptions(base_options=base_options,
+                                       num_hands=1)
+        return vision.HandLandmarker.create_from_options(options)
--- ml/src/utils.py
+++ ml/src/utils.py
@@ -1,6 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
+import mediapipe as mp
+from mediapipe import solutions
+from mediapipe.framework.formats import landmark_pb2
+import cv2
 
 
 def get_worst_preds(batch_gen, Y_pred, threshold):
@@ -51,3 +55,44 @@ def crop_resize_image(img, target=(224, 224)):
     # Crop the center of the image
     img = img.crop((left, top, right, bottom))
     return img.resize(target, Image.ANTIALIAS)
+
+def draw_landmarks_on_image(rgb_image, detection_result):
+    """ Draws hand landmarks on top of the image """
+    margin = 10  # pixels
+    font_size = 1
+    font_thickness = 1
+    handedness_text_color = (88, 205, 54) # vibrant green
+
+    hand_landmarks_list = detection_result.hand_landmarks
+    handedness_list = detection_result.handedness
+    annotated_image = np.copy(rgb_image)
+
+    # Loop through the detected hands to visualize.
+    for idx, _ in enumerate(hand_landmarks_list):
+        hand_landmarks = hand_landmarks_list[idx]
+        handedness = handedness_list[idx]
+
+        # Draw the hand landmarks.
+        hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+        hand_landmarks_proto.landmark.extend([
+        landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks])
+        solutions.drawing_utils.draw_landmarks(
+        annotated_image,
+        hand_landmarks_proto,
+        solutions.hands.HAND_CONNECTIONS,
+        solutions.drawing_styles.get_default_hand_landmarks_style(),
+        solutions.drawing_styles.get_default_hand_connections_style())
+
+        # Get the top left corner of the detected hand's bounding box.
+        height, width, _ = annotated_image.shape
+        x_coordinates = [landmark.x for landmark in hand_landmarks]
+        y_coordinates = [landmark.y for landmark in hand_landmarks]
+        text_x = int(min(x_coordinates) * width)
+        text_y = int(min(y_coordinates) * height) - margin
+
+        # Draw handedness (left or right hand) on the image.
+        cv2.putText(annotated_image, f"{handedness[0].category_name}",
+                    (text_x, text_y), cv2.FONT_HERSHEY_DUPLEX,
+                    font_size, handedness_text_color, font_thickness, cv2.LINE_AA)
+
+    return annotated_image

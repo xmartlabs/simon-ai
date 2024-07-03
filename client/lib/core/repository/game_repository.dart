a13845bfc3944a9dae482b1e958c6cbe9c1da91b
Federico Correa <fedecor27@gmail.com>
2024-07-03 12:59:01 -0300

    feat: game repository

---
+++ client/lib/core/repository/game_repository.dart
@@ -0,0 +1,62 @@
+import 'dart:async';
+
+import 'package:rxdart/rxdart.dart';
+import 'package:simon_ai/core/model/hand_gestures.dart';
+
+class GameRepository {
+  final StreamController<HandGesutre> _gameController = BehaviorSubject();
+
+  final StreamController<SequenceStatus> _sequenceController =
+      BehaviorSubject();
+
+  List<HandGesutre> recognizedGestures = [];
+
+  List<HandGesutre> currentSequence = [];
+
+  int points = 0;
+
+  Stream<HandGesutre> get gameStream => _gameController.stream;
+
+  Stream<SequenceStatus> get sequenceStream => _sequenceController.stream;
+
+  Future<void> takeSnapShot(HandGesutre gesture) async {
+    addRecognizedGesture(gesture);
+
+    if (currentSequence.length == recognizedGestures.length) {
+      _sequenceController.add(SequenceStatus.complete);
+      return;
+    }
+    if (currentSequence[recognizedGestures.length] != recognizedGestures.last) {
+      _sequenceController.add(SequenceStatus.wrong);
+    } else {
+      _sequenceController.add(SequenceStatus.correct);
+      points += 10;
+    }
+  }
+
+  void cacheCurrentSequence(List<HandGesutre> sequence) {
+    currentSequence = sequence;
+  }
+
+  void addGestureToSequence(HandGesutre gesture) {
+    currentSequence = [...currentSequence, gesture];
+  }
+
+  void addRecognizedGesture(HandGesutre gesture) {
+    recognizedGestures.add(gesture);
+    _gameController.add(gesture);
+  }
+
+  void dispose() {
+    _gameController.close();
+  }
+
+  void reset() {
+    recognizedGestures.clear();
+    points = 0;
+    currentSequence.clear();
+    _sequenceController.add(SequenceStatus.complete);
+  }
+}
+
+enum SequenceStatus { correct, wrong, incomplete, complete }

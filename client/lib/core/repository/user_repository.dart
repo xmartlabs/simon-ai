18f6d3cbc3ef4de629331e51c03c3faa6b6a86f5
Federico Correa <fedecor27@gmail.com>
2024-07-23 15:49:08 -0300

    feat: update user points

--- client/lib/core/repository/user_repository.dart
+++ client/lib/core/repository/user_repository.dart
@@ -7,6 +7,8 @@ class UserRepository {
   // ignore: unused_field
   final DbInterface<User> _userRemoteSource;
 
+  User? _user;
+
   final Stock<dynamic, List<User>?> _store;
 
   UserRepository(this._userRemoteSource)
@@ -16,9 +18,25 @@ class UserRepository {
           ),
         );
 
+  User get currentUser => _user!;
+
+  void setCurrentUser(User user) => _user = user;
+
   Stream<List<User>?> getUsers() => _store
       .stream(null)
       .mapToResult()
       .where((event) => event.isSuccess && event.data != null)
       .map((event) => event.data!.requireData());
+
+  Future<void> insertUser(User user) async =>
+      _userRemoteSource.insert('users', user);
+
+  Future<void> updateUser(User user) async =>
+      _userRemoteSource.update('users', user.email, user);
+
+  Future<void> deleteUser(String id) async =>
+      _userRemoteSource.delete('users', id);
+
+  Future<User?> getUser(String id) async =>
+      _userRemoteSource.getData('users', id);
 }

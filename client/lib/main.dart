5d2de7a46ba2af879ac504042f81c5b68466d699
mirland <mirland@xmartlabs.com>
2024-09-24 14:29:58 -0300

    feat: Feedback package integration

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 
+import 'package:design_system/theme/app_color_scheme.dart';
+import 'package:feedback/feedback.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -58,10 +60,16 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) => ScreenUtilInit(
-        designSize: const Size(1240, 773),
-        minTextAdapt: false,
-        splitScreenMode: true,
-        builder: (_, __) => const MainScreen(),
+  Widget build(BuildContext context) => BetterFeedback(
+        themeMode: ThemeMode.light,
+        theme: FeedbackThemeData(
+          colorScheme: AppColorScheme.getDefaultColorScheme(),
+        ),
+        child: ScreenUtilInit(
+          designSize: const Size(1240, 773),
+          minTextAdapt: false,
+          splitScreenMode: true,
+          builder: (_, __) => const MainScreen(),
+        ),
       );
 }
